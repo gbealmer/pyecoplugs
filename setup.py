70e916ba39d8673294e85c57c23e573e5acb6425
gbealmer <gbealmer@gmail.com>
2018-06-11 12:22:00 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyecoplug',
+        version='0.0.5',
+        description='Interface for the EcoPlug outlet',
+        url='http://github.com/philburr/pyecoplug',
+        author='phil.burr@gmail.com',
+        license='MIT',
+        packages=['pyecoplug'],
+        install_requires=[],
+        zip_safe=True)
